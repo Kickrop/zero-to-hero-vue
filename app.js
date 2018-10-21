493d4ff4f9264a752b0929c41e63fdb6d2822e6c
den <kickrop@gmail.com>
2018-10-21 23:33:40 +0300

    increment component

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+const navbar = Vue.component('navbar', {
+    template: `
+        <header>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+            </ul>
+        </header>
+        `,
+});
+const counter = Vue.component('counter', {
+    template: `
+    <span>
+        <button @click="this.increment">+</button>
+        <span>{{sum}}</span>
+        <button @click="this.decrement">-</button>
+    </span>`,
+    data: () => {
+        return {sum: 0};
+    },
+    methods: {
+        increment() {
+            this.sum += 1;
+        },
+        decrement() {
+            this.sum -= 1;
+        }
+    }
+});
 const app = new Vue({
     el: '#app',
     data: {
@@ -19,5 +48,9 @@ const app = new Vue({
         toggleMessage() {
             this.hasMessage = !this.hasMessage;
         },
+    },
+    components: {
+        navbar,
+        counter,
     },
 });
