73b2ec5220ebe637673fc13902a23980b3012650
den <kickrop@gmail.com>
2018-10-22 23:34:37 +0300

    clean up

--- app.js
+++ app.js
@@ -1,56 +1,44 @@
-const navbar = Vue.component('navbar', {
-    template: `
-        <header>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-            </ul>
-        </header>
-        `,
-});
-const counter = Vue.component('counter', {
-    template: `
-    <span>
-        <button @click="this.increment">+</button>
-        <span>{{sum}}</span>
-        <button @click="this.decrement">-</button>
-    </span>`,
-    data: () => {
-        return {sum: 0};
-    },
-    methods: {
-        increment() {
-            this.sum += 1;
-        },
-        decrement() {
-            this.sum -= 1;
-        }
-    }
-});
-const app = new Vue({
+import Vue from 'vue';
+import App from './src/App.vue';
+new Vue({
     el: '#app',
-    data: {
-        message: 'From Zero to Hero with Vue!',
-        hasMessage: true,
-        vue:
-            'Vue.js is an open-source JavaScript framework for building user interfaces.',
-        selected: '',
-        messages: [
-            {text: 'Hello world'},
-            {text: 'You are cool!'},
-            {text: 'twitter.com/hui'},
-         ],
-        },
-    methods: {
-        logMessage() {
-            console.log(this.message);
-        },
-        toggleMessage() {
-            this.hasMessage = !this.hasMessage;
-        },
-    },
-    components: {
-        navbar,
-        counter,
-    },
+    render: (h) => h(App),
 });
+// const navbar = Vue.component('navbar', {
+//     template: `
+//         <header>
+//             <ul>
+//                 <li>Home</li>
+//                 <li>About</li>
+//             </ul>
+//         </header>
+//         `,
+// });
+
+// const app = new Vue({
+//     el: '#app',
+//     data: {
+//         message: 'From Zero to Hero with Vue!',
+//         hasMessage: true,
+//         vue:
+//             'Vue.js is an open-source JavaScript framework for building user interfaces.',
+//         selected: '',
+//         messages: [
+//             {text: 'Hello world'},
+//             {text: 'You are cool!'},
+//             {text: 'twitter.com/hui'},
+//          ],
+//         },
+//     methods: {
+//         logMessage() {
+//             console.log(this.message);
+//         },
+//         toggleMessage() {
+//             this.hasMessage = !this.hasMessage;
+//         },
+//     },
+//     components: {
+//         navbar,
+//         counter,
+//     },
+// });
